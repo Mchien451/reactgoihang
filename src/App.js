d7195b8e17a4a706b7ff6400f85a3beacd133aec
Mchien451 <cuutrang0106@gmail.com>
2022-09-26 21:27:21 +0700

    p

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ExerciseCart from "./components/ExerciseCart";
+
+function App() {
+  return (
+    <div>
+      <ExerciseCart />
+    </div>
+  );
+}
+
+export default App;
